e000d3b90120d8f57fc14dd6fccb56709cfc7977
infinotiver <88645086+infinotiver@users.noreply.github.com>
2025-10-02 23:04:25 +0530

    refactor(api): add CORS middleware to enable cross-origin requests

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from backend.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Adding CORS middleware to allow cross-origin requests.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(router)
